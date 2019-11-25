4e441d76aab904c3fc38956846148d519f011868
zerobits01 <zerobits01@yahoo.com>
2019-11-25 17:35:47 +0330

    adding all models but methods of none of the models are tested

--- TourinoUser/admin.py
+++ TourinoUser/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import TourinoUser
+from .models import TourinoUser,TourComment,ProductComment,Wallet,ProductSell,TourSell
 # Register your models here.
 
 admin.site.register(TourinoUser)
+admin.site.register(ProductComment)
+admin.site.register(TourComment)
+admin.site.register(ProductSell)
+admin.site.register(TourSell)
+admin.site.register(Wallet)
+
--- TourinoUser/models.py
+++ TourinoUser/models.py
@@ -84,3 +84,116 @@ class TourinoUser(models.Model):
         data = jsondata.decode('utf-8')
         id = int(data['id'])
         user = User.objects.get(pk=int(id))
+
+
+class TourComment(models.Model):
+    rate = models.DecimalField(max_digits=2, decimal_places=1)
+    comment = models.TextField()
+    user = models.OneToOneField(TourinoUser, on_delete=models.CASCADE)
+    tour = models.OneToOneField(Tour, on_delete=models.CASCADE)
+
+    
+    def toDict(self):
+        data = {
+            'rate': self.rate,
+            'comment': self.comment,
+            'username': self.user__username,
+            'tour' : self.tour__id
+        }
+        return data
+
+    @classmethod
+    def newComment(cls, jsondata):
+        data = jsondata.decode('utf-8')
+        data = json.loads(data)
+        tour = Tour.objects.get(pk=int(data['tourid']))
+        tourcomment = TourinoUser.objects.get(pk=int(data['userid'])).tourcomment_set.create(
+            rate =  float(data['rate']),
+            comment= data['comment'],
+            tour= tour
+        )
+        return tourcomment.toDict()
+ 
+class ProductComment(models.Model):
+    rate = models.DecimalField(max_digits=2, decimal_places=1)
+    comment = models.TextField()
+    user = models.OneToOneField(TourinoUser, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    
+    def toDict(self):
+        data = {
+            'rate': self.rate,
+            'comment': self.comment,
+            'username': self.user__username,
+            'product' : self.product__id
+        }
+        return data
+
+    @classmethod
+    def newComment(cls, jsondata):
+        data = jsondata.decode('utf-8')
+        data = json.loads(data)
+        product = Product.objects.get(pk=int(data['productid']))
+        user = TourinoUser.objects.get(pk=int(data['userid']))
+        productcomment = cls(
+            rate= float(data['rate']),
+            comment= data['comment'],
+            product= product,
+            user=user
+        )
+        return productcomment.toDict()
+
+
+# for after adding payment options to it.
+class Wallet(models.Model):
+    user = models.OneToOneField(TourinoUser, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='ProductSell', blank=True, null=True)
+    tours = models.ManyToManyField(Tour, through='TourSell', blank=True, null=True)
+    amount = models.DecimalField(max_digits=20, decimal_places=9,blank=True, null=True)
+
+    @classmethod
+    def newWallet(cls,user):
+        # exactly with user register
+        wallet = cls(user=user)
+        return wallet
+        
+    def addToWallet(self):
+        # products bought before
+        pass
+
+    def toJson(self):
+            # TODO : json stringified dictionary
+        pass
+
+
+class ProductSell(models.Model):
+    wallet = models.ForeignKey(Wallet , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product , on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def toJson(self):
+            # TODO : json stringified dictionary
+        pass
+    
+    @classmethod
+    def newProductSell(cls,jsondata):
+        # TODO : creating ProductSell from json data
+        # sample : book = Book.create("Pride and Prejudice")
+        pass
+
+
+class TourSell(models.Model):
+    wallet = models.ForeignKey(Wallet , on_delete=models.CASCADE)
+    tour = models.ForeignKey(Tour , on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def toJson(self):
+            # TODO : json stringified dictionary
+        pass
+    
+    @classmethod
+    def newTourSell(cls,jsondata):
+        # TODO : creating new TourSell from json data
+        pass
+
+# in next versions working on popular and saving all users data
