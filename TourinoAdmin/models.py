75f2368b2c6a580aff6aa20c8539acdcea12e78b
zerobits01 <zerobits01@yahoo.com>
2019-11-25 16:30:46 +0330

    adding admin area successfuly

--- TourinoAdmin/models.py
+++ TourinoAdmin/models.py
@@ -1,3 +1,61 @@
 from django.db import models
-
+from django.core.signing import Signer
 # Create your models here.
+
+
+class TourinoAdmin(models.Model):
+    Roles = [
+        ('A', "Admin"),      # admin can do everything
+        ('T', "TourLeader"),  # just making Tours
+        ('S', "Seller"),     # publishing new products
+        ('B', "Blogger")     # publishing new articles about lorestan and rimale
+    ]
+    email = models.EmailField()
+    phone = models.CharField(max_length=13)
+    username = models.CharField(max_length=15, unique=True)
+    password = models.TextField()
+    role = models.CharField(max_length=1, choices=Roles)
+
+    def toDict(self):
+        '''
+            gets no input args
+            this returns dictionary(email,phone,username(unique),role)
+        '''
+        data = {
+            'email': self.email,
+            'phone': self.phone,
+            'username': self.username,
+            'role': self.role
+        }
+        return data
+
+    # @classmethod
+    # def register(cls, jsondata):
+        '''
+            gets json data should contains
+                {
+                    email,
+                    password,
+                    role from choices('A','B','S','T')admin,blogger,seller,tourleader
+                }
+            returns dictionary of added admin in database(email,phone,username(unique),role)
+        '''
+    #     data = jsondata.decode('utf-8')
+    #     data = json.loads(data)
+    #     admin = cls(email=data['email'],
+    #                 password=signer.sign(['password']),
+    #                 username=data['email'],
+    #                 role=data['role'],
+    #                 phone=data['phone']
+    #                 )
+    #     return admin.toDict()
+
+    def signIn(self, candidate):
+        '''
+            input argument : candidate password to check
+            returns Boolean if the hashed password is equal or not
+        '''
+        # userpwd = signer.unsign(self.password)
+        if self.password != candidate:
+            return False
+        return True
--- TourinoAdmin/views.py
+++ TourinoAdmin/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import request,JsonResponse
 # Create your views here.
+
+def home():
+    return JsonResponse({
+        'msg' : 'home page'
+    },status=200)
